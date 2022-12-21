5ede770d82f86730a5ddebedf85c553d4cca074a
platinumpizza29 <kbelgi1@gmail.com>
2022-12-21 01:36:26 +0000

    create and delete goals

--- models/goal_model.js
+++ models/goal_model.js
@@ -2,7 +2,17 @@ const router = require("express").Router();
 const { LexModelBuildingService } = require("aws-sdk");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/saathiDB");
+mongoose.connect("mongodb://localhost/saathiDB").catch((err) => {
+  if (err) {
+    console.log("====================================");
+    console.log("error while connecting to mongoDB", err);
+    console.log("====================================");
+  } else {
+    console.log("====================================");
+    console.log("Successfully Connected to DB");
+    console.log("====================================");
+  }
+});
 
 const goalSchema = new mongoose.Schema({
   goal_id: Number,
@@ -19,25 +29,80 @@ const goalSchema = new mongoose.Schema({
 
 const Goal = mongoose.model("Goals", goalSchema);
 
+var calcEndDate = (duration, type, createTime) => {
+  var goalDate = new Date(createTime);
+  var date = new Date();
+  if (type == "weeks") {
+    return date.setDate(goalDate + 7 * duration);
+  } else if (type == "months") {
+    return date.setDate(goalDate + 31 * duration);
+  } else if (type == "years") {
+    return date.setDate(goalDate + 365 * duration);
+  }
+};
+
+var calcDurationAmount = (type, amount, duration) => {
+  var intAmount = parseInt(amount);
+  var newDuration = parseInt(duration);
+  if (type == "weeks") {
+    return intAmount / newDuration;
+  } else if (type == "month") {
+    if (newDuration == 1) {
+      return intAmount / 4;
+    }
+    return intAmount / newDuration;
+  } else if (type == "year") {
+    if (newDuration == 1) {
+      return intAmount / 12;
+    }
+    return intAmount / newDuration;
+  }
+};
+
 router.post("/createGoal", (req, res) => {
   //this goal creation route
+  // need to add method for goal Duration Amount
+  // need to add method for goal End Date
+  //need to add goal Status : default -> Active
+  //need to change amount and duration from string to int
+  console.log(req.body);
+  var goalName = req.body.goalName;
+  var goalImageUrl = req.body.goalImageUrl;
+  var goalAmount = req.body.goalAmount;
+  var goalDuration = req.body.goalDuration;
+  var goalDurationType = req.body.goalDurationType;
+  var goalDurationAmount = calcDurationAmount(
+    goalDurationType,
+    goalAmount,
+    goalDuration
+  ); // create a method
+  var goalCreatedDate = req.body.goalCreatedDate;
+  var goalEndDate = calcEndDate(
+    goalDuration,
+    goalDurationType,
+    goalCreatedDate
+  ); // calculate end date
+  var goalCurrency = req.body.goalCurrency;
+  var goalState = "Active";
   const newGoal = new Goal({
-    goal_id: 1,
-    goalName: "test",
-    goalImageUrl:
-      "https://images.unsplash.com/photo-1671006327209-52aa2bb83072?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    goalAmount: 1000,
-    goalCurrency: "Rupees",
-    goalDurationAmount: 200,
-    goalDuration: 5,
-    goalDurationType: "months",
-    goalCreatedDate: "18/12/2022",
-    goalState: "Active",
+    goalName: goalName,
+    goalImageUrl: goalImageUrl,
+    goalAmount: goalAmount,
+    goalCurrency: goalCurrency,
+    goalDurationAmount: goalDurationAmount,
+    goalDuration: goalDuration,
+    goalDurationType: goalDurationType,
+    goalCreatedDate: goalCreatedDate,
+    goalEndDate: goalEndDate,
+    goalState: goalState,
   })
     .save()
     .then(
-      () => console.log("Goal Added"),
-      res.statusCode(200),
+      () => {
+        console.log("Goal Added");
+        res.sendStatus(200);
+      },
+      //   res.statusCode(200),
       (err) => console.log(err)
     );
 });
@@ -46,11 +111,19 @@ router.post("/updatePost", (req, res) => {
   // this is to update a specific goal
 });
 
-router.post("/deletePost", (req, res) => {
+router.post("/deleteGoal", (req, res) => {
   // this is to delete a specific goal
+  var id = req.body.id;
+  Goal.findByIdAndDelete(id, (err, docs) => {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 });
 
-router.get("/allgoals", (req, res) => {
+router.get("/allGoals", (req, res) => {
   Goal.find({}, (err, found) => {
     if (!err) {
       res.send(found).status(200);
@@ -59,7 +132,9 @@ router.get("/allgoals", (req, res) => {
       console.log(err);
       console.log("====================================");
     }
-  }).catch((err) => console.log("Some Error Occurred", err));
+  })
+    .clone()
+    .catch((err) => console.log("Some Error Occurred", err));
 });
 
 module.exports = router;
